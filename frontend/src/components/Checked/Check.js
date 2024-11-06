1f013bac3cf978e0e98a192664779226f8d494cc
Joacim Thenander <info@microlab.se>
2024-11-06 14:34:26 +0100

    Add checked component

---
+++ frontend/src/components/Checked/Check.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { CheckLg } from "react-bootstrap-icons";
+import classes from "./Check.module.scss";
+
+function Check() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShow(true), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      {show && (
+        <div className={classes["outer-wrapper"]}>
+          <div className={classes.bg} />
+          <div className={classes.wrapper}>
+            <CheckLg color="#000" />
+            <div className={classes.revealer} />
+            <div className={classes.border} />
+          </div>
+          <div className={classes["border-revealer"]} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Check;
