805ac91f94b0945b6505c629bfc2d4012ceb509a
Joacim Thenander <info@microlab.se>
2024-11-09 12:10:16 +0100

    Lägg till body-parser ochc 404

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,27 @@
 import express from "express";
 import { errorHandler } from "./middlewares.js";
 import routes from "./routes/index.js";
-import cors from "cors";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Ladda miljövariabler från .env-filen
+dotenv.config();
 
 const app = express();
 
 const corsOptions = {
-  origin: process.env.API_ORIGIN,
+  origin: process.env.API_ORIGIN || "http://localhost:3000", // Fallback om API_ORIGIN inte är definierad
 };
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use("/", routes);
 
+// Error handler som sista middleware
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
